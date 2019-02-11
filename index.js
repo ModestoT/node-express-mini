06a667a6338c081b357a79bd9cc0c4e4a9dd06b0
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-11 15:12:34 -0600

    Added the end-point for getting a user by their id

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ server.post('/api/users', (req, res) => {
             res.status(201).json({ success: true, user });
         })
         .catch( err => {
-            res.status(500).json({ success: false, error: "There was an error while saving the user to the database" })
+            res.status(500).json({ success: false, error: "There was an error while saving the user to the database" });
         })
 });
 
@@ -33,7 +33,24 @@ server.get('/api/users', (req, res) => {
             res.status(200).json({ success: true, users})
         })
         .catch( err => {
-            res.status(500).json({ success: false, error: "The users information could not be retrieved." })
+            res.status(500).json({ success: false, error: "The users information could not be retrieved." });
+        })
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    db
+        .findById(id)
+        .then(user => {
+            if(user) {
+                res.status(200).json({ success: true, user })
+            } else {
+                res.status(404).json({ success: false, message: "The user with the specified ID does not exist." });
+            }
+        })
+        .catch( err => {
+            res.status(500).json({ success: false, error: "The user information could not be retrieved." });
         })
 })
 
